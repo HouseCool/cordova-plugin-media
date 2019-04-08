23da4ad983186f2bf989bd2e619f09a9973efef9
[username] <tanhonglei@baishijiaju.com>
2019-04-08 10:33:15 +0800

    更新播放语音

--- src/android/AudioHandler.java
+++ src/android/AudioHandler.java
@@ -1,21 +1,3 @@
-/*
-       Licensed to the Apache Software Foundation (ASF) under one
-       or more contributor license agreements.  See the NOTICE file
-       distributed with this work for additional information
-       regarding copyright ownership.  The ASF licenses this file
-       to you under the Apache License, Version 2.0 (the
-       "License"); you may not use this file except in compliance
-       with the License.  You may obtain a copy of the License at
-
-         http://www.apache.org/licenses/LICENSE-2.0
-
-       Unless required by applicable law or agreed to in writing,
-       software distributed under the License is distributed on an
-       "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-       KIND, either express or implied.  See the License for the
-       specific language governing permissions and limitations
-       under the License.
-*/
 package org.apache.cordova.media;
 
 import org.apache.cordova.CallbackContext;
@@ -42,6 +24,9 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
 
 /**
  * This class called by CordovaActivity to play and record audio.
@@ -323,13 +308,14 @@ public class AudioHandler extends CordovaPlugin {
      * @param file				The name of the audio file.
      */
     public void startPlayingAudio(String id, String file,String PlayTheMode) {
-        for(String key:players.keySet())
-        {
-            AudioPlayer audio = players.remove(id);
-            if (players.isEmpty()) {
-                onLastPlayerReleased();
-            }
-            audio.destroy();
+        Set<Map.Entry<String, AudioPlayer>> set=players.entrySet();
+        Iterator<Map.Entry<String, AudioPlayer>> iterator=set.iterator();
+        while(iterator.hasNext()){
+            Map.Entry<String, AudioPlayer> entry=iterator.next();
+            String name = entry.getKey();
+            AudioPlayer value = entry.getValue();
+            iterator.remove();
+            value.destroy();
         }
         AudioManager audioManager = (AudioManager)this.cordova.getContext().getSystemService(Context.AUDIO_SERVICE);
         if(PlayTheMode == "receiver"){   //切换为听筒模式
